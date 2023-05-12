11e273abc9a8846bc0abe72477dae31191e0dbf1
Usakshi-code14 <106445047+ughadesakshi14@users.noreply.github.com>
2023-05-12 17:18:58 +0530

    12th may work completed

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let searchBtn=documnet.queryselector('#search-btn');
 let searchar=documnet.queryselector('#search-bar-container');
+let formbtn=documnet.queryselector('#login-btn');
+let loginForm=documnet.queryselector('#login-bar-container');
+let formClose=documnet.queryselector('#form-close');
+
 
 
 window.onscroll=()=>{
@@ -9,4 +13,12 @@ window.onscroll=()=>{
 searchBtn.addEventListner('click',()=>{
     searchBtn.classlist.toggle('fa-times');
     searchBtn.classlist.toggle('active');
+});
+
+formBtn.addEventListner('click',()=>{
+    loginForm.classlist.add('active');
+});
+
+formClose.addEventListner('click',()=>{
+    loginForm.classlist.remove('active');
 });
