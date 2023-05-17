775a9a61c68fdb5e742ad41654d4d0905a75dfcb
Usakshi-code14 <106445047+ughadesakshi14@users.noreply.github.com>
2023-05-18 00:06:16 +0530

    18-05-2023

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ let searchar=documnet.queryselector('#search-bar-container');
 let formbtn=documnet.queryselector('#login-btn');
 let loginForm=documnet.queryselector('#login-bar-container');
 let formClose=documnet.queryselector('#form-close');
-
+let menu=document.queryselector('#menu-bar');
+let navbar=document.querySelector('#.navbar')
 
 
 window.onscroll=()=>{
-    //searchBtn.classlist.remove('fa-times');
-    //searchBtn.classlist.remove('active');
+    searchBtn.classlist.remove('fa-times');
+    searchBar.classlist.remove('active');
+    menu.classlist.remove('fa-times');
+    navbar.classlist.remove('active');
+    loginForm.classlist.remove('active');
 }
+
+menu.addEventListner('click',() =>{
+menu.classlist.toggle('fa-times');
+navbar.classlist.toggle('active');
+});
+
+
 searchBtn.addEventListner('click',()=>{
     searchBtn.classlist.toggle('fa-times');
     searchBtn.classlist.toggle('active');
